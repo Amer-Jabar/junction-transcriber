13bf27a2d484700b3c52052603e3a66370bbb115
Bogdan Tischenko <tischenkob@gmail.com>
2025-10-05 09:39:33 +0200

    Async processing

--- junction-backend/app.py
+++ junction-backend/app.py
@@ -1,16 +1,23 @@
 import os
 import tempfile
+import json
 from flask import Flask, request, jsonify
-from werkzeug.utils import secure_filename
 from dotenv import load_dotenv
 from flask_cors import CORS
 import pika
 import minio
+import uuid
+from pymongo import MongoClient
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 # Load environment variables
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "../config/.env"))
 
 FLASK_SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "supersecretkey")
+
+logging.info("initializing flask")
 
 # Flask setup
 app = Flask(__name__)
@@ -18,6 +25,16 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 app.config["SECRET_KEY"] = FLASK_SECRET_KEY
 app.config["UPLOAD_FOLDER"] = tempfile.gettempdir()
 app.config["MAX_CONTENT_LENGTH"] = 100 * 1024 * 1024  # 100MB max upload
+
+
+logging.info("initializing mongo")
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/transcriber")
+mongo_client = MongoClient(MONGO_URI)
+db = mongo_client.get_database()
+transcripts_collection = db.transcripts
+
+logging.info("initializing rabbitmq")
 
 rmqConnection = pika.BlockingConnection(pika.ConnectionParameters('localhost', credentials=pika.PlainCredentials("admin", "password")))
 rmqChannel = rmqConnection.channel()
@@ -27,7 +44,9 @@ rmqChannel.queue_declare(queue=detection_tasks_queue_name)
 transcriptions_tasks_queue_name='transcriptions_tasks'
 rmqChannel.queue_declare(queue=transcriptions_tasks_queue_name)
 
-#connect to minio with user and password
+
+logging.info("initializing minio")
+
 minioClient = minio.Minio("localhost:9000", secure=False, access_key="admin", secret_key="password")
 
 audio_bucket_name = "files"
@@ -38,49 +57,46 @@ transcriptions_bucket_name = "transcriptions"
 if not minioClient.bucket_exists(transcriptions_bucket_name):
     minioClient.make_bucket(transcriptions_bucket_name)
 
-rmqChannel.start_consuming()
-
-
 @app.route("/api/transcribe", methods=["POST"])
 def upload_and_transcribe():
-    if "audio" not in request.files:
+    logging.info(f"Received {request}")
+    if "audio" not in request.files or request.files["audio"].filename == "":
         return jsonify({"error": "No audio file provided"}), 400
-
     audio_file = request.files["audio"]
-    if audio_file.filename == "":
-        return jsonify({"error": "No selected file"}), 400
-
-    filename = secure_filename(audio_file.filename)
-    temp_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-    audio_file.save(temp_path)
 
     try:
-        # TODO
-        pass
+        # save the file to minio files bucket with file size
+        minioClient.put_object(audio_bucket_name, audio_file.filename, audio_file, length=-1, part_size=10*1024*1024)
+
+        # generate uuid
+        id = str(uuid.uuid4())
+
+        rmqChannel.basic_publish(exchange='',
+                              routing_key=transcriptions_tasks_queue_name,
+                              body=json.dumps({
+                                  "id": id,
+                                  "filename": audio_file.filename
+                              }))
+
+        return jsonify({"filename": audio_file.filename,"transcript_id": id}), 200
     except Exception as e:
+        logging.error(e)
         return jsonify({"error": str(e)}), 500
-    finally:
-        if os.path.exists(temp_path):
-            os.remove(temp_path)
 
 @app.route("/api/transcript/<transcript_id>", methods=["GET"])
 def get_transcript(transcript_id):
-    from bson.objectid import ObjectId
-
     try:
-        doc = transcripts_collection.find_one({"_id": ObjectId(transcript_id)})
+        doc = transcripts_collection.find_one({"id": transcript_id})
         if not doc:
             return jsonify({"error": "Transcript not found"}), 404
-        doc["_id"] = str(doc["_id"])
+        del doc["_id"]
         return jsonify(doc)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-
 @app.route("/")
 def index():
-    return jsonify({"message": "The app is running."})
-
+    return jsonify({"message": "Whisper Demo API is running."})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8090)
--- junction-whisper/app.py
+++ junction-whisper/app.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import json
 import tempfile
@@ -9,7 +10,9 @@ from flask_cors import CORS
 import pika
 import minio
 
-from category_classifier import HateSpeechPredictor
+from .category_classifier import HateSpeechPredictor
+
+logging.basicConfig(level=logging.INFO)
 
 # Load environment variables
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "../config/.env"))
@@ -18,15 +21,20 @@ MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/transcriber")
 FLASK_SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "supersecretkey")
 WHISPER_MODEL = os.getenv("WHISPER_MODEL", "roberta-large")  # You can set this in .env
 
+tempdirectory = tempfile.gettempdir()
+
 # MongoDB setup
+logging.info("Initializing mongo")
 mongo_client = MongoClient(MONGO_URI)
 db = mongo_client.get_database()
 transcripts_collection = db.transcripts
 
 # Whisper model (load once)
+logging.info("Initializing whisper")
 whisper_model = whisper.load_model(WHISPER_MODEL)
 predictor = HateSpeechPredictor()
 
+logging.info("Initializing rabbimq")
 rmqConnection = pika.BlockingConnection(pika.ConnectionParameters('localhost', credentials=pika.PlainCredentials("admin", "password")))
 rmqChannel = rmqConnection.channel()
 detection_tasks_queue_name='detection_tasks'
@@ -34,20 +42,19 @@ rmqChannel.queue_declare(queue=detection_tasks_queue_name)
 transcriptions_tasks_queue_name='transcriptions_tasks'
 rmqChannel.queue_declare(queue=transcriptions_tasks_queue_name)
 
-def transcribe(path: str, filename: str):
-    segments = transcribe_audio(path)
-    transcript_doc = {
-        "filename": filename,
-        "segments": segments,
-    }
-    result = transcripts_collection.insert_one(transcript_doc)
 
-    transcript_id = str(result.inserted_id)
-    transcript_doc["_id"] = transcript_id
+def transcribe(id: str, path: str, filename: str):
+    logging.info(f"Transcribing {filename}")
+    transcript_doc = {
+        "id": id,
+        "filename": filename,
+        "segments": transcribe_audio(path),
+    }
+    _ = transcripts_collection.insert_one(transcript_doc)
 
     rmqChannel.basic_publish(exchange='',
                           routing_key=detection_tasks_queue_name,
-                          body=json.dumps({"transcript_id": transcript_id, "filename": filename}))
+                          body=json.dumps({"id": id, "filename": filename}))
 
 def transcribe_audio(file_path):
     result = whisper_model.transcribe(file_path, no_speech_threshold=5)
@@ -72,12 +79,14 @@ def transcribe_audio(file_path):
 
 def on_transcription_task(connection, method, properties, message):
     data = json.loads(message.decode('UTF-8'))
+    logging.info(f"Received {message}")
+    id = data["id"]
     filename = data["filename"]
 
-    temp_path = os.path.join(tempfile.gettempdir(), filename)
+    temp_path = os.path.join(tempdirectory, filename)
     minioClient.fget_object(audio_bucket_name, filename, temp_path)
 
-    transcribe(temp_path, filename)
+    transcribe(id, temp_path, filename)
 
 rmqChannel.basic_consume(queue=transcriptions_tasks_queue_name, on_message_callback=on_transcription_task, auto_ack=True)
 
